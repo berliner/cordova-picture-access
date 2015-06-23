247a90ef7cf555ca0cb5df74294adbb9480a6711
berliner <myberliner@gmail.com>
2015-06-23 11:11:55 +0200

    Handle first time access and ALAuthorizationStatusNotDetermined

--- src/ios/PictureAccess.m
+++ src/ios/PictureAccess.m
@@ -19,6 +19,26 @@
         result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"Access granted"];
         NSLog(@"Access to picture gallery granted");
     }
+    else if (authStatus == ALAuthorizationStatusNotDetermined) {
+        NSLog(@"Access to picture gallery not yet determined");
+        __block CDVPluginResult* result = nil;
+
+        ALAssetsLibrary *lib = [[ALAssetsLibrary alloc] init];
+        [lib enumerateGroupsWithTypes:ALAssetsGroupSavedPhotos usingBlock:^(ALAssetsGroup *group, BOOL *stop) {
+            result = [CDVPluginResult resultWithStatus:CDVCommandStatus_OK messageAsString:@"Access not determined"];
+            NSLog(@"Access to picture gallery granted");
+        } failureBlock:^(NSError *error) {
+            if (error.code == ALAssetsLibraryAccessUserDeniedError) {
+                result = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"Access denied"];
+                NSLog(@"Access to picture gallery denied");
+            } else{
+                result = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"Access denied"];
+                NSLog(@"Other error code: %i", error.code);
+            }
+        }];
+
+
+    }
     else {
         result = [CDVPluginResult resultWithStatus:CDVCommandStatus_ERROR messageAsString:@"Access denied"];
         NSLog(@"Access to picture gallery denied");
